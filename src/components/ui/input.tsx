14614abff4016437007bb48fb0656900039abcfa
Kallox <d34d.mc@gmail.com>
2024-09-23 21:50:28 -0300

    Fix[TSConfig]: Implicit any false

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils"
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
-    hint: string
+    hint?: string
   }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
