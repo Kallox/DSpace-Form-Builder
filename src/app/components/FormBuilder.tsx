d951778382472316362ba0e3a8f7b06b3ad3abbe
Kallox <d34d.mc@gmail.com>
2024-10-02 16:17:21 -0300

    Feat[Add element button]: Removed drag and drop to add element, button created

--- src/app/components/FormBuilder.tsx
+++ src/app/components/FormBuilder.tsx
@@ -12,20 +12,6 @@ import { XmlUploadModal } from './XmlUploadModal'
 import { FormElementSettingsModal } from './FormElementSettingsModal'
 import { FormElement, FormRow, Form } from '@/types/Form'
 
-const initialFormElements = [
-  { id: 'input', type: 'onebox', label: 'Text' },
-  { id: 'textarea', type: 'textarea', label: 'Text Area' },
-  { id: 'select', type: 'dropdown', label: 'Select' },
-  { id: 'date', type: 'date', label: 'Date' },
-  { id: 'tag', type: 'tag', label: 'Tag' },
-  { id: 'group', type: 'group', label: 'Modal' },
-  { id: 'inlinegroup', type: 'inline-group', label: 'Inline form' },
-  { id: 'name', type: 'name', label: 'Person Name' },
-  { id: 'link', type: 'link', label: 'Link' },
-  { id: 'series', type: 'series', label: 'Publication Series' },
-
-]
-
 interface FormBuilderProps {
   form: FormRow[];
   formName: string;
@@ -42,34 +28,7 @@ export default function EnhancedFormBuilder({ form, formName, onFormChange, onFo
 
     const { source, destination } = result
 
-    if (source.droppableId === 'elements' && destination.droppableId.startsWith('row')) {
-      const baseElement = initialFormElements.find(el => el.id === result.draggableId);
-      const newElement: FormElement = {
-        id: `${result.draggableId}-${Date.now()}`,
-        schema: '',
-        element: '',
-        qualifier: '',
-        label: '',
-        inputType: baseElement ? baseElement.type : '',
-        repeatable: false,
-        required: '',
-        hint: '',
-        style: '',
-        typeBind: '',
-        regex: '',
-        vocabulary: '',
-        vocabularyClosed: false,
-        visibility: true,
-        readonly: false,
-        valuePairsName: ''
-      }
-      
-      const rowIndex = parseInt(destination.droppableId.split('-')[1])
-      const newForm = [...form]
-      newForm[rowIndex].elements.splice(destination.index, 0, newElement)
-      setSavedForm(null)
-      onFormChange(newForm)
-    } else if (source.droppableId.startsWith('row') && destination.droppableId.startsWith('row')) {
+    if (source.droppableId.startsWith('row') && destination.droppableId.startsWith('row')) {
       const sourceRowIndex = parseInt(source.droppableId.split('-')[1])
       const destRowIndex = parseInt(destination.droppableId.split('-')[1])
       
@@ -219,6 +178,33 @@ export default function EnhancedFormBuilder({ form, formName, onFormChange, onFo
     onFormChange([...form, { id: `row-${Date.now()}`, elements: [] }])
   }
 
+  const addNewElement = (rowIndex: number) => {
+    const newElement: FormElement = {
+      id: `${Date.now()}`,
+      schema: '',
+      element: '',
+      qualifier: '',
+      label: '',
+      inputType: '',
+      repeatable: false,
+      required: '',
+      hint: '',
+      style: '',
+      typeBind: '',
+      regex: '',
+      vocabulary: '',
+      vocabularyClosed: false,
+      visibility: true,
+      readonly: false,
+      valuePairsName: ''
+    }
+    const newForm = [...form]
+    const elementsLength = newForm[rowIndex].elements.length
+    newForm[rowIndex].elements.splice(elementsLength, 0, newElement)
+    setSavedForm(null)
+    onFormChange(newForm)
+  }
+
   const moveRowUp = (index: number) => {
     if (index > 0) {
       const newForm = [...form]
@@ -244,31 +230,7 @@ export default function EnhancedFormBuilder({ form, formName, onFormChange, onFo
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="flex h-screen">
-        <div className="w-1/6 bg-gray-100 p-4">
-          <h2 className="text-lg font-bold mb-4">Form Elements</h2>
-          <Droppable droppableId="elements" isDropDisabled={true}>
-            {(provided) => (
-              <div {...provided.droppableProps} ref={provided.innerRef}>
-                {initialFormElements.map((element, index) => (
-                  <Draggable key={element.id} draggableId={element.id} index={index}>
-                    {(provided) => (
-                      <div
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        className="bg-white p-2 mb-2 rounded shadow"
-                      >
-                        {element.label}
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        </div>
-        <div className="w-5/6 p-4">
+        <div className="w-full p-4">
           <div className="flex justify-between items-center mb-4">
             <Input
               id="FormName"
@@ -285,7 +247,7 @@ export default function EnhancedFormBuilder({ form, formName, onFormChange, onFo
           {form.map((row, rowIndex) => (
             <Droppable key={row.id} droppableId={`row-${rowIndex}`}>
               {(provided) => (
-                <div className="relative min-h-[100px] border-2 border-dashed p-4 mb-4">
+                <div className="relative min-h-[20px] border-2 border-dashed p-4 mb-4">
                   <span className="absolute top-0 left-0 bg-gray-200 text-xs px-2 py-1 rounded-br">Row</span>
                   <div className="absolute top-0 right-0 flex">
                     <Button
@@ -312,7 +274,7 @@ export default function EnhancedFormBuilder({ form, formName, onFormChange, onFo
                   <div
                     {...provided.droppableProps}
                     ref={provided.innerRef}
-                    className="min-h-[100px]"
+                    className="min-h-[20px]"
                   >
                     {row.elements.map((element, index) => (
                       <Draggable key={element.id} draggableId={element.id} index={index}>
@@ -330,6 +292,9 @@ export default function EnhancedFormBuilder({ form, formName, onFormChange, onFo
                       </Draggable>
                     ))}
                     {provided.placeholder}
+                    <div className="border-2 border-dashed w-full mt-4">
+                      <Button variant="outline" className="w-full" onClick={() => addNewElement(rowIndex)}><PlusCircle color="gray"/></Button>
+                    </div>
                   </div>
                 </div>
               )}
